4879a43d4f75e10410ebf37922a3a64b8110a02b
mobssie <mobssie.jung@gmail.com>
2021-04-06 22:33:27 +0900

    home container search bar

--- src/page/home/HomeContainer.js
+++ src/page/home/HomeContainer.js
@@ -7,23 +7,43 @@
  */
  import 'react-native-gesture-handler';
  import React, {Component} from 'react';
- import {View, Text, Button} from 'react-native';
- import { Header } from 'react-native-elements';
- 
+ import {View, Text, Button, SafeAreaView, StatusBar} from 'react-native';
+ import { SearchBar } from 'react-native-elements';
+
+
  class HomeContainer extends Component {
+    state = {
+      search: '',
+    };
+
+    updateSearch = (search) => {
+      this.setState({ search });
+    };
    render() {
+     const { search } = this.state;
      return (
      <>
-      
-       <View style={{
-          flex:4,
-          alignItems:'center',
-          justifyContent: 'center'
-        }}
-       >
-          
-           <Text>HomeContainer Screen!!!</Text>
-       </View>
+        <StatusBar/>
+          <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
+          <View style={{ flex: 1 }}>
+            <SearchBar
+              placeholder="파트너를 검색해보세요."
+              onChangeText={this.updateSearch}
+              value={search}
+            />
+          </View>
+            
+          <View style={{
+              flex:4,
+              alignItems:'center',
+              justifyContent: 'center'
+            }}
+          >
+            
+            
+            <Text>HomeContainer Screen!!!</Text>
+        </View>
+       </SafeAreaView>
        </>
      )
    }
