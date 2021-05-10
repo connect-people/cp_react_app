761cdb95f6d9e9e57bcca9f616ab57bcceb7b8dd
mobssie <mobssie.jung@gmail.com>
2021-05-10 14:37:19 +0900

    api 모듈

--- src/API/api.js
+++ src/API/api.js
@@ -3,6 +3,27 @@ import axios from 'axios';
 const key = '80CFeBE4MD6JmhEfClBx7zqo1eGvwTl5EZgKyMQc'
 const host_api = 'https://api.connect-people.club'
 const token ='eyJhbGciOiJIUzUxMiIsImlhdCI6MTYxNzc3MDIzNCwiZXhwIjoxNjE3NzcwMjM0fQ.eyJtZW1iZXJfaWQiOjF9.fobbDXZODd-IwuSKtNbZgbkzfH6ntT3FrxLY_V6OWdyhLF__RYqFAc0tl3srE6vGbMgERkiiYYYVJBoiGR1K6Q'
+
+
+const DOMAIN = '';
+const UNAUTHORIZED = 401;
+const onUnauthorized = () => {
+
+}
+
+const request = (method, url, data) => {
+    return axios({
+        method,
+        url: DOMAIN + url,
+        data
+    }).then(result => result.data)
+      .catch(result => {
+
+      })
+}
+
+
+
 // 검색
 export const callSearchKeyword = async (page, perPage, keyword) => {
     try {
@@ -26,7 +47,7 @@ export const callCMajorCategory = async () => {
     try {
         return await axios.get(`${host_api}/v1/category/major`, {
             'x-api-key': key,
-            'token': token,
+            'token': token, 
         })
 
     } catch (error) {
@@ -34,6 +55,7 @@ export const callCMajorCategory = async () => {
 
     }
 }
+
 // 카테고리 중소
 export const callCMinorCategory = async () => {
     try {
--- src/config/index.js
+++
@@ -1,2 +0,0 @@
-export const NETWORK_INTERFACE = 'http://localhost:8080/';
-export const HOST_API = 'http://52.78.173.151';
